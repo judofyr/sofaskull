6028b20c50426013b52b369f4f6da27ccfb9cde8
Magnus Holm <judofyr@gmail.com>
2008-06-22 17:05:48 +0200

    Switching to a dependency-system, instead of include all Neko-helpers

--- lib/sofaskull.rb
+++ lib/sofaskull.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
+require 'set'
 require 'treetop'
 begin
   require 'sofaskullparser' 
@@ -79,20 +80,27 @@ module SofaSkull
   end
 
   class Program
+    DEPENDENCIES = {
+      :order => [:cells, :subs, :print, :get_cell, :set_cell, :read,
+                 :copy_cell, :set_sub, :call_sub],
+      :get_cell => :cells,
+      :set_cell => :get_cell,
+      :copy_cell => :set_cell,
+      :read => :set_cell,
+      :print => :get_cell,
+      :set_sub => :subs,
+      :call_sub => :subs,
+    }
     EOF = 0
-    attr_reader :result, :statements, :clean
-    attr_accessor :io_mode
+    attr_reader :statements, :clean
+    attr_accessor :io_mode, :dependencies
  
     def initialize(source, sub = false)
       @statements = parse(source)
       @sub = sub
       @io_mode = :asc
-      @result = String.new
-      header! unless sub
-    end
-
-    def header!
-      @result << File.read(File.join(File.dirname(__FILE__), "helpers.neko"))
+      @result = String.new 
+      @dependencies = Set.new
     end
  
     def step
@@ -102,22 +110,30 @@ module SofaSkull
       when IOMode
         @io_mode = s.to_sym 
       when ModCell
+        depends_on :set_cell
         op = "-" if s.op == :-
         mod = (!s.op.nil?).to_s
         @result << "set_cell(#{s.cell}, #{op}#{s.value}, #{mod});"
       when CopyCell
+        depends_on :copy_cell
         @result << "copy_cell(#{s.cell}, #{s.to});"
-      when PrintCell
+      when PrintCell    
+        depends_on :print
         @result << "$print(sprintf(#{ps}, get_cell(#{s.cell})));"
-      when ReadCell
+      when ReadCell    
+        depends_on :read
         @result << "read_cell(#{s.cell}, #{io_mode == :asc});"
-      when WhileBlock
+      when WhileBlock  
+        depends_on :get_cell
         @result << "while(get_cell(#{s.cell})!=0){#{sub(s.statements.elements)}}"
       when AddSub
+        depends_on :set_sub
         @result << "set_sub(#{s.sub}, function(){#{sub(s.statements.elements)}});"
       when RunSub
+        depends_on :call_sub
         @result << "call_sub(#{s.sub});"
       when ConRunSub
+        depends_on :get_cell, :call_sub
         @result << "if(get_cell(#{s.cell})==0){call_sub(#{s.sub})}"
       end
       true
@@ -141,6 +157,29 @@ module SofaSkull
         raise SofaSkull::SyntaxError
       end
     end
+    
+    def depends_on(*d)
+      @dependencies.merge(d.each do |x|
+        more = [DEPENDENCIES[x]].flatten.compact
+        unless @dependencies.include?(x) && more.empty?
+          depends_on(*more)
+        end
+      end)
+    end
+    
+    def dependencies
+      @dependencies.to_a.sort_by { |x| DEPENDENCIES[:order].index(x) }
+    end
+    
+    def headers
+      dependencies.map do |dep|
+        File.read(File.join(File.dirname(__FILE__), "helpers", "#{dep}.neko"))
+      end.join($/)
+    end
+      
+    def result(with_headers = true)
+       (with_headers ? headers : "") + @result
+    end
 
     # Removes comments and spaces
     def self.clean(s)
@@ -152,8 +191,10 @@ module SofaSkull
     def sub(code)
       p = self.class.new(code.dup, true)
       p.io_mode = io_mode
+      p.dependencies = @dependencies
       p.run
-      p.result
+      @dependencies = p.dependencies.to_set
+      p.result(false)
     end
   end
 end
